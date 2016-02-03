964df93bef593c07afb4f5089c670726e6f1edec
Sujan Khadka <sujank@uab.edu>
2016-02-03 10:47:13 -0600

    Added rules 1 to 6

--- simplify.scm
+++ simplify.scm
@@ -16,7 +16,9 @@
   (lambda (exp)
     (let ((op (car exp)) (e1 (getlhs exp)) (e2 (getrhs exp)))
       (cond
-       ((and (number? e1) (number? e2)) (reduce op e1 e2))
+       ((eq? op '*) (simplifyMultiplication e1 e2))
+       ((and (isTerm e1) (number? e2)) (swapNumberTerm op e1 e2)) ;2, 4, 6
+       ((and (number? e1) (number? e2)) (reduce op e1 e2)) ;1, 3, 5
        (else
 	(let ((s1 (simplify e1)) (s2 (simplify e2)))
 	  (simplify (list op s1 s2))
@@ -27,15 +29,19 @@
     )
   )
 
-(define getlhs
-  (lambda (exp)
-    (cadr exp)
-    )
+(define simplifyMultiplication
+  (lambda (t1 t2)
+    (
+     
+     )
   )
 
-(define getrhs
-  (lambda (exp)
-    (car (cddr exp))
+(define swapNumberTerm
+  (lambda (op t c)
+    (if (eq? '- op)
+	(list '+ (- 0 c) t)
+	(list op c t)
+	)
     )
   )
 
@@ -49,5 +55,26 @@
 	('* (* n1 n2))
 	)
       )
+    )
+  )
+
+
+;;;an expression is a term if it is a list OR
+;;;an expression is a term if it is a variable like x (a symbol)
+(define isTerm
+  (lambda (exp)
+    (or (list? exp) (symbol? exp))
+    )
+  )
+
+(define getlhs
+  (lambda (exp)
+    (cadr exp)
+    )
+  )
+
+(define getrhs
+  (lambda (exp)
+    (car (cddr exp))
     )
   )
