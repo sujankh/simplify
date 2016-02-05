94542ddb9c7641e327d6e3585fada9fd37794551
S Abhishek Anand <anandab@uab.edu>
2016-02-05 16:24:38 -0600

    added rules

--- tests.scm
+++ tests.scm
@@ -17,10 +17,12 @@
 (check '(* t1 (* t2 t3)) '(* (* t1 t2) t3)) ;test8
 (check '(+ (+ 5 t) 7)  '(+ 12 t))           ;test9
 (check '(* (* 5 t) 4) '(* 20 t)) ;test10
-(check '(* (+ 9 t) 2) '(+ 18 (* 2 t)))
-(check '(* 5 (+ 7 t)) '(+ 35 (* 5 t))) ;(∗c1 (+c2 t)) → (+(∗c1 c2)(∗c1 t))
-(check '(* (+ t1 t2) 5) '(+ (* 5 t1)(* 5 t2)))  ;(∗(+t1 t2)c) → (+(∗ct1)(∗ct2))
-(check '(* 10 (+ t1 t2)) '(+ (* 10 t1)(* 10 t2)))    ;(∗c(+t1 t2)) → (+(∗ct1)(∗ct2))
+(check '(* (+ 9 t) 2) '(+ 18 (* 2 t))) ;test11
+(check '(* 5 (+ 7 t)) '(+ 35 (* 5 t))) ;test12
+(check '(* (+ t1 t2) 5) '(+ (* 5 t1)(* 5 t2)))  ;test13
+(check '(* 10 (+ t1 t2)) '(+ (* 10 t1)(* 10 t2)))    ;test14
+(check '(* (- t1 t2) 5) '(- (* 5 t1) (* 5 t2))) ;test15
+(check '(* 5 (- t1 t2)) '(- (* 5 t1) (* 5 t2))) ;test16
 (check '(* (+ a b) c) '(+ (* a c) (* b c))) ;test17
 (check '(* (- a b) c) '(- (* a c) (* b c))) ;test19
 (check '(* a (+ b c)) '(+ (* a b) (* a c))) ;rule18
